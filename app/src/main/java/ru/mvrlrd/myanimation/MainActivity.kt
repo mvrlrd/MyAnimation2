a64bef6142e503ef1ca1f357e5ae61d56bd2bffd
mvrlrd <igrbelykh@gmail.com>
2022-02-22 15:32:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/mvrlrd/myanimation/MainActivity.kt
@@ -0,0 +1,41 @@
+package ru.mvrlrd.myanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import ru.mvrlrd.myanimation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.image1.setOnClickListener {
+            it.isSelected = !it.isSelected
+//            binding.motionContainer.transitionToEnd()
+        }
+        binding.image2.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+        binding.image3.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+        binding.image4.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+        binding.image5.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+        binding.image6.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        binding.motionContainer.transitionToStart()
+        return super.onTouchEvent(event)
+    }
+}
